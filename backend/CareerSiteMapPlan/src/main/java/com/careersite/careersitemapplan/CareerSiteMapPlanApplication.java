376ef2ff39288dab5788f3dc6aef61efb7d94705
Nmazza1 <52589448+Nmazza1@users.noreply.github.com>
2022-12-12 18:58:00 -0500

    Initialization of Backend

---
+++ backend/CareerSiteMapPlan/src/main/java/com/careersite/careersitemapplan/CareerSiteMapPlanApplication.java
@@ -0,0 +1,13 @@
+package com.careersite.careersitemapplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerSiteMapPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerSiteMapPlanApplication.class, args);
+	}
+
+}
---
+++ backend/CareerSiteMapPlan/src/test/java/com/careersite/careersitemapplan/CareerSiteMapPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careersite.careersitemapplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerSiteMapPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
